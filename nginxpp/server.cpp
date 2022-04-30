ba2d42ab42fcc66e0341b7335b78396d46846b63
Yang Yang <yyang.even@gmail.com>
2022-04-29 23:56:50 -0700

    Server: Implement accept
    Change-Id: Idbebaf012c31d5e23b0f96ac96ce02de0aaa6696

--- nginxpp/server.cpp
+++ nginxpp/server.cpp
@@ -1,20 +1,23 @@
 #include <nginxpp/server.hpp>
 
 #include <atomic>
+#include <iostream>
+#include <thread>
 
 #include <csignal>
 #include <errno.h>
 #include <string.h>
-#include <unistd.h>
-#include <fcntl.h>
 
+#include <arpa/inet.h>
+#include <fcntl.h>
 #include <netdb.h>
 #include <netinet/tcp.h>
+#include <poll.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <unistd.h>
 
 #include <cxxopts.hpp>
-#include <gsl/gsl>
 
 #include <nginxpp/exception.hpp>
 
@@ -32,11 +35,54 @@ extern "C" void signalHandler(int signal) {
 }
 
 template<typename... Args>
-inline constexpr void
-setSocketOption(Args &&... args) {
+inline constexpr void setSocketOption(Args &&... args) {
     if (setsockopt(std::forward<Args>(args)...) == -1) {
         throw SocketException("Failed to setsockopt(): "s + strerror(errno));
     }
+}
+
+[[nodiscard]]
+auto getPort(const sockaddr_storage &address) {
+    if (address.ss_family == AF_INET) {
+        return ntohs(reinterpret_cast<const sockaddr_in *>(&address)->sin_port);
+    } else if (address.ss_family == AF_INET6) {
+        return ntohs(reinterpret_cast<const sockaddr_in6 *>(&address)->sin6_port);
+    } else {
+        throw SocketException("Failed to get port: Unsupported address family");
+    }
+}
+
+[[nodiscard]]
+gsl::not_null<const void *> locateInternetAddress(const sockaddr_storage &address) {
+    if (address.ss_family == AF_INET) {
+        return &(reinterpret_cast<const sockaddr_in *>(&address)->sin_addr);
+    } else if (address.ss_family == AF_INET6) {
+        return &(reinterpret_cast<const sockaddr_in6 *>(&address)->sin6_addr);
+    } else {
+        throw SocketException("Failed to locate internet address: Unsupported address family");
+    }
+}
+
+template<typename Function, typename... Args>
+[[nodiscard]]
+inline constexpr auto handleEINTR(const Function func, Args &&... args) noexcept {
+    Expects(func);
+
+    int result{};
+    do {
+        result = func(std::forward<Args>(args)...);
+    } while (result < 0 and errno == EINTR);
+
+    return result;
+}
+
+[[nodiscard]]
+inline auto pollOne(pollfd &pfd, const std::chrono::milliseconds &timeout) noexcept {
+    return handleEINTR(poll, &pfd, 1, timeout.count());
+}
+
+inline auto hasConnectionRequest(pollfd &pfd) noexcept {
+    return 0 != pollOne(pfd, ServerOptions::accept_timeout);
 }
 
 }//namespace
@@ -66,32 +112,31 @@ Socket createServerSocket(const ServerOptions &options) {
     const int yes = 1;
     std::stringstream errors;
     for (auto *p = servinfo; p; p = p->ai_next) {
-        const auto socket_fd = socket(p->ai_family, p->ai_socktype, p->ai_protocol);
-        if (socket_fd == Socket::INVALID_SOCKET) {
+        Socket sock{socket(p->ai_family, p->ai_socktype, p->ai_protocol)};
+        if (sock == Socket::INVALID_SOCKET) {
             errors << "Failed to create socket: " << strerror(errno) << '\n';
             continue;
         }
-        Socket s(socket_fd);
 
-        if (fcntl(socket_fd, F_SETFD, FD_CLOEXEC) == -1) {
+        if (fcntl(sock, F_SETFD, FD_CLOEXEC) == -1) {
             continue;
         }
 
-        setSocketOption(socket_fd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes));
+        setSocketOption(sock, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes));
         if (ServerOptions::tcp_nodelay) {
-            setSocketOption(socket_fd, IPPROTO_TCP, TCP_NODELAY, &yes, sizeof(yes));
+            setSocketOption(sock, IPPROTO_TCP, TCP_NODELAY, &yes, sizeof(yes));
         }
 
-        if (bind(socket_fd, p->ai_addr, p->ai_addrlen) == -1) {
+        if (bind(sock, p->ai_addr, p->ai_addrlen) == -1) {
             errors << "Failed to bind(): " << strerror(errno) << '\n';
             continue;
         }
 
-        if (listen(socket_fd, ServerOptions::listen_backlog) == -1) {
+        if (listen(sock, ServerOptions::listen_backlog) == -1) {
             throw SocketException("Failed to listen(): "s + strerror(errno));
         }
 
-        return s;
+        return sock;
     }
 
     throw SocketException("Failed to create server socket: " + errors.str());
@@ -106,13 +151,7 @@ int getPort(const Socket &socket) {
         throw SocketException("Failed to getsockname(): "s + strerror(errno));
     }
 
-    if (address.ss_family == AF_INET) {
-        return ntohs(reinterpret_cast<sockaddr_in *>(&address)->sin_port);
-    } else if (address.ss_family == AF_INET6) {
-        return ntohs(reinterpret_cast<sockaddr_in6 *>(&address)->sin6_port);
-    } else {
-        throw SocketException("Failed to get port: Unsupported address family");
-    }
+    return ::getPort(address);
 }
 
 }//namespace internal
@@ -167,21 +206,67 @@ void HttpServer::greet() const noexcept {
     "Listening on port: " << m_options.port << std::endl;
 }
 
+
 bool HttpServer::Run() const noexcept {
     greet();
 
     (void) std::signal(SIGINT, signalHandler);  // Handle 'Ctrl+c'
     (void) std::signal(SIGQUIT, signalHandler); // Handle 'Ctrl+\'
 
+    sockaddr_storage their_address{};
+    char address_buffer[INET6_ADDRSTRLEN] = {};
+    pollfd server_pfd;
+    server_pfd.fd = m_socket;
+    server_pfd.events = POLLIN;
     for (;;) {
         if (g_signal) {
             std::cout << "Caught signal " << strsignal(g_signal) <<
                 '(' << g_signal << "), shutting down..." << std::endl;
             break;
         }
+
+        if (not hasConnectionRequest(server_pfd))
+            continue;
+
+        socklen_t address_size = sizeof(their_address);
+        Socket sock{accept(m_socket, reinterpret_cast<sockaddr *>(&their_address), &address_size)};
+        if (sock == Socket::INVALID_SOCKET) {
+            if (errno == EMFILE) {
+                std::cerr << "Open file descriptors limit reached. Waiting for available spots." << std::endl;
+                std::this_thread::sleep_for(ServerOptions::accept_timeout);
+                continue;
+            }
+
+            std::cerr << "Failed to accept(): " << strerror(errno) << std::endl;
+            return false;
+        }
+
+        inet_ntop(their_address.ss_family, locateInternetAddress(their_address), address_buffer, sizeof(address_buffer));
+        onAccept(std::move(sock), address_buffer, getPort(their_address));
     }
 
     return true;
+}
+
+void HttpServer::onAccept(Socket sock, const gsl::not_null<gsl::czstring> address, const int port) const noexcept {
+    static std::atomic<unsigned> session_created{};
+
+    Expects(sock != Socket::INVALID_SOCKET);
+
+    const auto session_id = session_created++;
+    std::cout << "Accepted new connection from: " << address <<
+        "; Port: " << port <<
+        "; FD: " << sock <<
+        "; Session: " << session_id << std::endl;
+
+    timeval timeout{};
+    timeout.tv_sec = ServerOptions::read_timeout.count();
+    timeout.tv_usec = 0;
+    setSocketOption(sock, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof(timeout));
+
+    timeout.tv_sec = ServerOptions::write_timeout.count();
+    timeout.tv_usec = 0;
+    setSocketOption(sock, SOL_SOCKET, SO_SNDTIMEO, &timeout, sizeof(timeout));
 }
 
 }//namespace nginxpp
